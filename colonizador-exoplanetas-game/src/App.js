078c06b928fb156ed1016860e70706348674194e
luwu <lucivp22@gmail.com>
2024-10-05 19:08:30 -0400

    Boton de jugar

--- colonizador-exoplanetas-game/src/App.js
+++ colonizador-exoplanetas-game/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home.js';
 
 function App() {
-  return (
-    <Home></Home>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- colonizador-exoplanetas-game/src/components/Home.js
+++ colonizador-exoplanetas-game/src/components/Home.js
@@ -1,34 +1,49 @@
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import './Home.css';
 import TextBlock from './textBlock';
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate('/otra-pagina');
+    };
+
     return (
-        <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
+        <Parallax pages={2} style={{ top: '0', left: '0' }} className="animation">
           <ParallaxLayer offset={0} speed={0.25}>
-            <div class="animation_layer parallax" id="ESPACIO"></div>
+            <div className="animation_layer parallax" id="ESPACIO"></div>
           </ParallaxLayer>
           <ParallaxLayer offset={0} speed={0.40}>
-            <div class="animation_layer parallax" id="NEBULOSA1"></div>
+            <div className="animation_layer parallax" id="NEBULOSA1"></div>
           </ParallaxLayer>
           <ParallaxLayer offset={0} speed={0.5}>
-            <div class="animation_layer parallax" id="NEBULOSA2"></div>
+            <div className="animation_layer parallax" id="NEBULOSA2"></div>
           </ParallaxLayer>
           <ParallaxLayer offset={0} speed={0.35}>
-            <div class="animation_layer parallax" id="NAVE"></div>
+            <div className="animation_layer parallax" id="NAVE"></div>
           </ParallaxLayer>
           <ParallaxLayer offset={0} speed={0.25}>
-            <div class="animation_layer parallax" id="PLANETA"></div>
+            <div className="animation_layer parallax" id="PLANETA"></div>
           </ParallaxLayer>
           <ParallaxLayer offset={0} speed={0.25}>
-            <div class="animation_layer parallax" id="HUMO"></div>
+            <div className="animation_layer parallax" id="HUMO"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.25}>
+            <div className="animation_layer parallax" id="BOTON-PARTE">
+                {/* Llama a la función handleButtonClick al hacer clic en el botón */}
+                <button className="Button" id="BOTON" onClick={handleButtonClick}>
+                    JUGAR
+                </button>
+            </div>
           </ParallaxLayer>
           <ParallaxLayer offset={1} speed={0.25}>
             <TextBlock />
           </ParallaxLayer>
         </Parallax>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
