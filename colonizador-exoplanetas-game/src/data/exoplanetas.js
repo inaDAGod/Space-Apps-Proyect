ca9df3a2a4ec151782ce347711afe363738fb6b2
AndyLaffertt <alaffertt@gmail.com>
2024-10-05 12:36:39 -0400

    archivo js incompleto

---
+++ colonizador-exoplanetas-game/src/data/exoplanetas.js
@@ -0,0 +1,29 @@
+const exoplanetas = [
+    {
+      nombre: "11 Comae Berenices B",
+      imagen: "11_Comae_Berenices_b.jpg",
+      consistencia: "",
+      temperatura: "",
+      liquido: "",
+      propAdmosfericas: "",
+      campMagnetico: "",
+      masaRadio: "",
+      distanciaTierra: "304 años luz",
+      solLunas:"",
+      distanciaASuSol:"",
+      OrbitaSol: ""
+    },
+    {
+      nombre: "Proxima Centauri b",
+      imagen: "proxima-centauri-b.jpg",
+      descripcion: "Un exoplaneta potencialmente habitable que orbita la estrella más cercana al Sol."
+    },
+    {
+      nombre: "TRAPPIST-1d",
+      imagen: "trappist-1d.jpg",
+      descripcion: "Uno de los siete planetas que orbitan la estrella TRAPPIST-1, con posibilidades de albergar agua."
+    }
+  ];
+  
+  export default exoplanetas;
+  
