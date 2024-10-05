d97b8e8c02f2d639e254accd422dae699d0f6d6b
JoshNisth <134982802+JoshNisth@users.noreply.github.com>
2024-10-05 16:30:20 -0400

    modificaciones en el navbar y boton para regresar

--- colonizador-exoplanetas-game/src/App.js
+++ colonizador-exoplanetas-game/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-
+import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* quitar ese style cuando ya tengamos el home */}
       <main style={{ minHeight: '100vh', backgroundColor: 'black'}}>
+      <ScrollToTop />
+      <div style={{ height: '1500px' }}></div> {/* Esto crea espacio para scroll */}
       </main>
       <Footer />
+      <ScrollToTop />
     </div>
   );
 }
--- colonizador-exoplanetas-game/src/components/Navbar.js
+++ colonizador-exoplanetas-game/src/components/Navbar.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';  // Asegúrate de tener este archivo para estilos personalizados.
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+  let lastScrollY = window.scrollY;
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setIsVisible(false);  // Oculta el navbar al hacer scroll hacia abajo
+      } else {
+        setIsVisible(true);   // Muestra el navbar al hacer scroll hacia arriba
+      }
+      lastScrollY = window.scrollY;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isVisible ? '' : 'hidden'}`}>
       <div className="navbar-container">
         <a href="#home" className="navbar-logo">NasaAmigos</a>
         <div className={`navbar-menu ${isOpen ? 'active' : ''}`}>
