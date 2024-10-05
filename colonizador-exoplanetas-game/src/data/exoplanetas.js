4b0349f1887d8a3cd134f004e835cb801a71ccc5
AndyLaffertt <alaffertt@gmail.com>
2024-10-05 15:04:59 -0400

    exoplanetas, aún incompleto

--- colonizador-exoplanetas-game/src/data/exoplanetas.js
+++ colonizador-exoplanetas-game/src/data/exoplanetas.js
@@ -11,23 +11,10 @@ const exoplanetas = [
         distanciaTierra: "304 años luz",
         lunas: "1-10 ", 
         distanciaASuSol: "1.178 AU", // Información proporcionada
-        OrbitaSol: "323.2 días" 
+        OrbitaSol: "323.2 días",
+        probSupervivencia:"14%"
       }
       ,
-    {
-        nombre: "HD 220074 b",
-        imagen: "HD 220074_b.png", 
-        consistencia: "Mayormente Gas Gigante",
-        temperatura: "≈ -130°C", // Un gigante gaseoso a 1.6 AU de un M-type star probablemente será frío
-        liquido: "Posiblemente no", // Similar a otros gigantes gaseosos, no se espera que tenga líquidos en la superficie
-        propAdmosfericas: "Hidrógeno y Helio", // Típico en gigantes gaseosos
-        campMagnetico: "Fuerte", // Gigantes gaseosos tienden a tener campos magnéticos notables
-        masaRadio: "16.64 veces la masa de Júpiter", // Basado en la información proporcionada
-        distanciaTierra: "1058 años luz", // No se menciona, pero estimo cientos de años luz
-        lunas: "1-10",
-        distanciaASuSol: "1.6 AU", // Proporcionado
-        OrbitaSol: "1.8 años" // Proporcionado
-      },
       {
         nombre: "BD+20 594 b",
         imagen: "BD+20_594_b.png", 
@@ -40,9 +27,85 @@ const exoplanetas = [
         distanciaTierra: "585 años luz", 
         lunas: "0-5",
         distanciaASuSol:  "0.241 AU", // Proporcionado
-        OrbitaSol: "41.7 días" 
+        OrbitaSol: "41.7 días",
+        probSupervivencia:"3%" 
       },
-
+      {
+        nombre: "DP Leonis b",
+        imagen: "DP_Leonis_b.png",
+        consistencia: "Mayormente Gas Gigante",
+        temperatura: "≈ -80°C",
+        liquido: "Posiblemente no",
+        propAdmosfericas: "Hidrógeno y Helio",
+        campMagnetico: "Moderado",
+        masaRadio: "6.28 veces la masa de Júpiter",
+        distanciaTierra: "997 años luz",
+        lunas: "1-10",
+        distanciaASuSol: "8.19 AU",
+        OrbitaSol: "28 años",
+        probSupervivencia:"13%"
+      }
+      ,
+      {
+        nombre: "GJ 86 b",
+        imagen: "GJ 86 b.png",
+        consistencia: "Mayormente Gas Gigante",
+        temperatura: "≈ 150°C",
+        liquido: "Posiblemente no",
+        propAdmosfericas: "Hidrógeno y Helio",
+        campMagnetico: "Moderado",
+        masaRadio: "4.42 veces la masa de Júpiter",
+        distanciaTierra: "35 años luz",
+        lunas: "1-5",
+        distanciaASuSol: "0.11 AU",
+        OrbitaSol: "15.8 días",
+        probSupervivencia:"14%"
+      },
+      {
+        nombre: "GJ 504 b",
+        imagen: "GJ_504_b.png",
+        consistencia: "Mayormente Gas Gigante",
+        temperatura: "≈ 700°C",
+        liquido: "Posiblemente no",
+        propAdmosfericas: "Hidrógeno, Helio y Vapor de Agua",
+        campMagnetico: "Desconocido",
+        masaRadio: "4.5 veces la masa de Júpiter",
+        distanciaTierra: "57 años luz",
+        lunas: "0-3",
+        distanciaASuSol: "43.5 AU",
+        OrbitaSol: "2.3 años",
+        probSupervivencia:"69%"
+      },
+      {
+        nombre: "GJ 3929 b",
+        imagen: "GJ_3929_b.png",
+        consistencia: "Mayormente Rocoso",
+        temperatura: "≈ 70°C", // Temperatura aproximada adecuada para la vida en la zona habitable
+        liquido: "Posiblemente sí", // Podría haber agua líquida en la superficie
+        propAdmosfericas: "Nitrógeno y Oxígeno", // Composición atmosférica estimada
+        campMagnetico: "Desconocido", // No se tiene información clara sobre el campo magnético
+        masaRadio: "1.75 veces la masa de la Tierra", 
+        distanciaTierra: "52 años luz",
+        lunas: "1-3", // Estimación basada en su masa y características
+        distanciaASuSol: "0.0252 AU", 
+        OrbitaSol: "2.6 días",
+        probSupervivencia: "22%" 
+      },
+    {
+        nombre: "HD 220074 b",
+        imagen: "HD 220074_b.png", 
+        consistencia: "Mayormente Gas Gigante",
+        temperatura: "≈ -130°C", // Un gigante gaseoso a 1.6 AU de un M-type star probablemente será frío
+        liquido: "Posiblemente no", // Similar a otros gigantes gaseosos, no se espera que tenga líquidos en la superficie
+        propAdmosfericas: "Hidrógeno y Helio", // Típico en gigantes gaseosos
+        campMagnetico: "Fuerte", // Gigantes gaseosos tienden a tener campos magnéticos notables
+        masaRadio: "16.64 veces la masa de Júpiter", // Basado en la información proporcionada
+        distanciaTierra: "1058 años luz", // No se menciona, pero estimo cientos de años luz
+        lunas: "1-10",
+        distanciaASuSol: "1.6 AU", // Proporcionado
+        OrbitaSol: "1.8 años", 
+        probSupervivencia:"54%"
+      }
       
   ];
   
