bdf7ecd031d1dfe02ff363355387a05bf07566f1
luwu <lucivp22@gmail.com>
2024-10-05 13:24:05 -0400

    Inicio animación

--- colonizador-exoplanetas-game/src/components/Home.js
+++ colonizador-exoplanetas-game/src/components/Home.js
@@ -1,10 +1,24 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import './Home.css';
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-    
-  )
+    return (
+        <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
+          <ParallaxLayer offset={0} speed={2.5}>
+            <div class="animation_layer parallax" id="ESPACIO"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={2.5}>
+            <div class="animation_layer parallax" id="NAVE"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={2.5}>
+            <div class="animation_layer parallax" id="NEBULOSA1"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={2.5}>
+            <div class="animation_layer parallax" id="NEBULOSA1"></div>
+          </ParallaxLayer>
+        </Parallax>
+    )
 }
 
 export default Home
