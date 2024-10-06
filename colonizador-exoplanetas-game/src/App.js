d42d08bc966baed918144f96365e5a030d350c35
unknown <ayana.siegle@ucb.edu.bo>
2024-10-05 21:02:39 -0400

    commet

--- colonizador-exoplanetas-game/src/App.js
+++ colonizador-exoplanetas-game/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importa BrowserRouter y Route
 import Home from './components/Home'; // Importa tu componente Home
 import Game from './components/Game'; // Importa tu componente Game (asegúrate de tener este componente)
+//import MyCarousel from './components/MyCarousel';
 
 const App = () => {
   return (
