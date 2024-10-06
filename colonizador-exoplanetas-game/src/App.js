6b796ee4baa072b1586c96f7e51fa4c0f52309bf
unknown <ayana.siegle@ucb.edu.bo>
2024-10-05 21:21:11 -0400

    nuevas respuestas
    exoplanet questions

--- colonizador-exoplanetas-game/src/App.js
+++ colonizador-exoplanetas-game/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importa BrowserRouter y Route
 import Home from './components/Home'; // Importa tu componente Home
 import Game from './components/Game'; // Importa tu componente Game (asegúrate de tener este componente)
-//import MyCarousel from './components/MyCarousel';
+import MyCarousel from './components/MyCarousel';
+//<Route path="/" element={<MyCarousel />} /> {/* Ruta para el componente Home */}
+//<Route path="/mycarousel" element={<MyCarousel />} /> {/* Ruta para el componente Game */}
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Game />} /> {/* Ruta para el componente Home */}
-        <Route path="/game" element={<Game />} /> {/* Ruta para el componente Game */}
+        <Route path="/" element={<MyCarousel />} /> {/* Ruta para el componente Home */}
+        <Route path="/mycarousel" element={<MyCarousel />} /> {/* Ruta para el componente Game */}
       </Routes>
     </Router>
   );
