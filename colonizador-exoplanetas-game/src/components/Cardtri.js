9f518a1f8e02723a0cd4565bda28b1f6e8182498
unknown <ayana.siegle@ucb.edu.bo>
2024-10-05 21:46:35 -0400

    css separado

--- colonizador-exoplanetas-game/src/components/Cardtri.js
+++ colonizador-exoplanetas-game/src/components/Cardtri.js
@@ -1,60 +1,16 @@
 import React from "react";
-import styled from "styled-components";
-
-const CardContainer = styled.div`
-  perspective: 1000px;
-`;
-
-const CardWrapper = styled.div`
-  width: 1000px;
-  height: 400px;
-  border-radius: 10px;
-  position: relative;
-  transition: transform 0.6s;
-  transform-style: preserve-3d;
-  cursor: pointer;
-
-  &.flipped {
-    transform: rotateY(180deg);
-  }
-`;
-
-const CardFace = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  backface-visibility: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: sans-serif;
-  font-size: 250%;
-  color: #fff;
-  padding-left: 2%;
-  padding-right: 2%;
-  background-color: #5c6992;
-  border-radius: 10px;
-`;
-
-const CardBack = styled(CardFace)`
-  transform: rotateY(180deg);
-  font-size: 250%;
-  padding-left: 2%;
-  padding-right: 2%;
-  background-color: ${(props) =>
-    props.noAnswer ? "#0c1428" : props.isCorrect ? "#28c9ac" : "#c92850"};
-`;
+import './Cardtri.css';
 
 const Cardtri = ({ question, detailedAnswer, isCorrect, noAnswer, showAnswer }) => {
   return (
-    <CardContainer>
-      <CardWrapper className={showAnswer ? "flipped" : ""}>
-        <CardFace>{question}</CardFace>
-        <CardBack isCorrect={isCorrect} noAnswer={noAnswer}>
+    <div className="card-container">
+      <div className={`card-wrapper ${showAnswer ? "flipped" : ""}`}>
+        <div className="card-face">{question}</div>
+        <div className={`card-back ${isCorrect ? "correct" : noAnswer ? "no-answer" : "incorrect"}`}>
           {detailedAnswer}
-        </CardBack>
-      </CardWrapper>
-    </CardContainer>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- colonizador-exoplanetas-game/src/components/MyCarousel.js
+++ colonizador-exoplanetas-game/src/components/MyCarousel.js
@@ -1,118 +1,8 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import './MyCarousel.css'; // Archivo CSS importado
 import exoplanetQuestions from "../data/exoplanetQuestions";
 import Card from "./Cardtri";
 import ProgressBar from "./ProgressBar";
-
-const Container = styled.div`
-  position: relative;
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const OptionsContainer = styled.div`
-  margin-top: 20px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 100%;
-  max-width: 950px;
-`;
-
-const AnswerButton = styled.label`
-  background-color: #836bcf;
-  color: white;
-  border-radius: 5px;
-  padding: 10px 20px;
-  margin: 5px;
-  text-align: center;
-  cursor: pointer;
-  transition: background-color 0.3s;
-  width: 45%;
-
-  input {
-    display: none;
-  }
-
-  &:hover {
-    background-color: #f0f0f0;
-    color: black;
-  }
-
-  &.selected {
-    background-color: #52c0f5;
-    color: #fff;
-  }
-`;
-
-const NextButtonContainer = styled.div`
-   background-color: #232289;
-  border-radius: 5px;
-  padding: 20px 40px;
-  margin: 5px;
-  font-size: 130%;
-  text-align: center;
-  cursor: pointer;
-  transition: background-color 0.3s;
-  width: 20%;
-  border: none;
-  color: white;
-
-  &:hover {
-    background-color: #127bb1;
-    color: white;
-  }
-`;
-
-const StyledButton = styled.button`
-  background-color: #232289;
-  border-radius: 5px;
-  padding: 20px 40px;
-  margin: 5px;
-  font-size: 130%;
-  text-align: center;
-  cursor: pointer;
-  transition: background-color 0.3s;
-  width: 20%;
-  border: none;
-  color: white;
-
-  &:hover {
-    background-color: #127bb1;
-    color: white;
-  }
-`;
-
-const ResultsModal = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-  text-align: center;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-`;
-
-const ModalButton = styled.button`
-  margin-top: 20px;
-  padding: 10px 20px;
-  background-color: #232289;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #127bb1;
-  }
-`;
 
 class MyCarousel extends Component {
   state = {
@@ -213,7 +103,7 @@ class MyCarousel extends Component {
     const correctAnswers = results.filter((result) => result.isCorrect).length;
 
     return (
-      <Container>
+      <div className="container">
         <ProgressBar
           totalQuestions={totalQuestions}
           resultsArray={resultsArray}
@@ -230,14 +120,14 @@ class MyCarousel extends Component {
               showAnswer={showAnswer}
             />
 
-            <OptionsContainer>
+            <div className="options-container">
               {!showAnswer &&
                 shuffledAnswers.map((answer, index) => (
-                  <AnswerButton
+                  <label
                     key={index}
-                    className={
+                    className={`answer-button ${
                       this.state.selectedAnswer === answer ? "selected" : ""
-                    }
+                    }`}
                   >
                     <input
                       type="radio"
@@ -246,33 +136,35 @@ class MyCarousel extends Component {
                       onChange={() => this.handleAnswerSelection(answer)}
                     />
                     {answer}
-                  </AnswerButton>
+                  </label>
                 ))}
-            </OptionsContainer>
+            </div>
 
             {showAnswer && (
-              <NextButtonContainer onClick={this.handleNext}>
+              <div className="next-button-container" onClick={this.handleNext}>
                 Siguiente
-              </NextButtonContainer>
+              </div>
             )}
 
             {!showAnswer && (
-              <StyledButton onClick={this.handleSkipQuestion}>
+              <button className="styled-button" onClick={this.handleSkipQuestion}>
                 Saltar
-              </StyledButton>
+              </button>
             )}
           </>
         ) : (
-          <ResultsModal>
+          <div className="results-modal">
             <h2>Resultados del Cuestionario</h2>
             <p>
               Respondiste correctamente {correctAnswers} de {totalQuestions}{" "}
               preguntas.
             </p>
-            <ModalButton onClick={this.handleRestart}>Reiniciar</ModalButton>
-          </ResultsModal>
+            <button className="modal-button" onClick={this.handleRestart}>
+              Reiniciar
+            </button>
+          </div>
         )}
-      </Container>
+      </div>
     );
   }
 }
